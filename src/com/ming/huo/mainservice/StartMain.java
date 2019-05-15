07aeb3afbc9e6d295dc7098ffa52b4b2163a2f99
minghuoxie <2300671708@qq.com>
2019-05-15 16:20:51 +0800

    commit,第一次提交代码

---
+++ src/com/ming/huo/mainservice/StartMain.java
@@ -0,0 +1,10 @@
+package com.ming.huo.mainservice;
+
+public class StartMain {
+    public static void main(String[] args) {
+        /***
+         * 服务端的主入口
+         * */
+        System.out.println("-----------hahahha");
+    }
+}
